e49be438e9080d5da9c4fa878679efa29b611e95
Abolade Olawuyi <aboladeolawuyi97@gmail.com>
2026-07-28 15:17:20 +0100

    Update route.js

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,6 +1,6 @@
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend("re_NmbtcMxJ_LCCwfVb8W9fjEdGfqeDRLVvA");
 
 
 export async function POST(request) {
